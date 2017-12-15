4112b9779ca3bff061c9bac8a6cdae4cf87b864f
Ben Vongtawan <Ben Vongtawan>
2017-12-15 01:28:20 -0800

    new repository

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public new GameObject camera;
+
+    public float Force = .05f;
+    public float JumpForce;
+
+    //public float dash;
+    private bool isSprint = false;
+
+    public float xForce;
+    public float yForce;
+    public float jump;
+
+    public bool canMove = true;
+    public bool canDash;
+    public bool groundCheck = true;
+
+    private Rigidbody playerRigidbody;
+
+    void Start()
+    {
+        playerRigidbody = gameObject.GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (!canMove)
+        {
+            return;
+        }
+        canDash = true;
+
+        transform.rotation = Quaternion.Euler(0, camera.GetComponent<MouseLook>().currentYRotation, 0);
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            isSprint = true;
+        }
+
+        xForce = Input.GetAxis("Horizontal") * Force;
+        yForce = Input.GetAxis("Vertical") * Force;
+
+        transform.Translate(new Vector3(0, jump, 0));
+
+
+        if (isSprint != true)
+        {
+            transform.Translate(new Vector3(xForce, 0, yForce));
+        }
+
+        else
+        {
+            transform.Translate(new Vector3(xForce * 2f, 0, yForce * 2f));
+        }
+        isSprint = false;
+
+        if (Input.GetKeyDown("space") && groundCheck)
+        {
+            playerRigidbody.AddForce(Vector3.up * 300);
+        }
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        groundCheck = true;
+    }
+
+    void OnCollisionStay(Collision other)
+    {
+        groundCheck = true;
+    }
+
+    void OnCollisionExit(Collision other)
+    {
+        groundCheck = false;
+    }
+}
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// Describes a type that can take damage.
+interface IDamageable
+{
+    // Estimates the amount of health lost if damage is dealt. 
+    float EstimatedDamageTaken(float damageDealt);
+
+    // Applies damage to this object.
+    void TakeDamage(float damageDealt);
+}
+
+
+public class PlayerHealth : MonoBehaviour, IDamageable
+{
+    public float currentHealth;
+    public float healthValue = 100;
+    //public float armorValue = 20;
+    public bool isDead = false;
+
+    public Image Health;
+    public Image damageImage;
+
+    public Color flashColor = new Color(1f, 0f, 0f, 0.1f);
+
+    public GameObject healEffect;
+
+    public float flashSpeed = 5f;
+    bool damaged;
+
+    public bool knockedBack;
+
+    void Start()
+    {
+        currentHealth = healthValue;
+        Health = GameObject.FindGameObjectWithTag("HP").GetComponent<Image>();
+    }
+
+    public float EstimatedDamageTaken(float damageDealt)
+    {
+        return damageDealt;
+    }
+
+
+    public void TakeDamage(float damageDealt)
+    {
+        healthValue -= damageDealt;
+
+        damaged = true;
+
+
+
+        if (Health.fillAmount <= 0)
+        {
+            Death();
+        }
+    }
+
+    public void Update()
+    {
+        Health.fillAmount = healthValue / 100;
+        if (isDead)
+        {
+            //Application.LoadLevel(Application.loadedLevel);
+        }
+
+        if (damaged)
+        {
+            // ... set the colour of the damageImage to the flash colour.
+           //damageImage.color = flashColor;
+        }
+        else
+        {
+            // ... transition the colour back to clear.
+            //damageImage.color = Color.Lerp(damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+        }
+        damaged = false;
+    }
+
+    void Death()
+    {
+        isDead = true;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Health")
+        {
+            if (healthValue < 100)
+            {
+                //healthValue += 10;
+                //Destroy(Instantiate(healEffect, gameObject.transform.position, gameObject.transform.rotation) as GameObject, 2);
+            }
+        }
+    }
+}
